3f03c66935e3d74fc0e8649fdd4f7f5d7c5bfa9f
danianre <danianre28@gmail.com>
2023-09-21 16:06:28 -0500

    Modificación

--- Granja/Controllers/InicioController.cs
+++ Granja/Controllers/InicioController.cs
@@ -3,6 +3,8 @@ using Granja.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Granja.Controllers
 {
@@ -27,11 +29,52 @@ namespace Granja.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Crear(Usuarios usuarios)
+        {
+            if (ModelState.IsValid) 
+            {
+                // Generar una contraseña aleatoria
+                usuarios.Password = GenerarPasswordAleatoria();
+
+                _contexto.Usuarios.Add(usuarios);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private string GenerarPasswordAleatoria()
+        {
+            // Longitud de la contraseña aleatoria (en este caso, 6 dígitos hexadecimales)
+            int longitud = 6;
+
+            // Caracteres permitidos en la contraseña
+            const string caracteresPermitidos = "0123456789ABCDEF";
+
+            // Crear un generador de números aleatorios seguro
+            using var rng = RandomNumberGenerator.Create();
+            byte[] bytesAleatorios = new byte[longitud];
+            rng.GetBytes(bytesAleatorios);
+
+            StringBuilder contraseñaAleatoria = new StringBuilder(longitud);
+
+            foreach (byte byteAleatorio in bytesAleatorios)
+            {
+                // Obtener un índice válido en función de la longitud de los caracteres permitidos
+                int indiceCaracter = byteAleatorio % caracteresPermitidos.Length;
+
+                // Agregar el carácter aleatorio a la contraseña
+                contraseñaAleatoria.Append(caracteresPermitidos[indiceCaracter]);
+            }
+
+            return contraseñaAleatoria.ToString();
         }
     }
 }
--- Granja/Models/Usuarios.cs
+++ Granja/Models/Usuarios.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlTypes;
 
 namespace Granja.Models
 {
@@ -7,15 +8,29 @@ namespace Granja.Models
     {
         [Key]
         public int IdUsuario { get; set; }
+        [Required (ErrorMessage = "Este campo es obligatorio")]
         public required string Nombre {  get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public required string Apellido { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public required int Edad { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public required int Contacto  { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public required DateTime Fecha_vinculacion { get; set; }
-        public required float Salario { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public required int Salario { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public required string Password { get; set; }
 
         [ForeignKey("Rol")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public int IdRol { get; set; }
         public required Roles Rol { get; set; }
     }
